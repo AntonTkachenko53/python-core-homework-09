9c7af256e911a1474bcc3345c65a27e433de67b4
AntonTkachenko53 <gogochristofour@gmail.com>
2023-09-11 23:25:59 +0300

    fix(not ready)

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 phone_book = {}
 
 
+def hello(*args):
+    return 'How can I help you?'
+
+
+def close(*args):
+    return "Good bye!"
+
+
+def show_all(*args):
+    result = ''
+    for key, value in phone_book.items():
+        result = result + '|{:^15}|{:^15}|\n'.format(key, value)
+    return result
+
+
 def add_or_change_phone(strip):
     words = strip.split(' ')
     if len(words) > 3:
         raise ValueError
     phone_book[words[1]] = words[2]
+    return 'Phone saved'
 
 
 def get_phone(strip):
@@ -14,23 +30,33 @@ def get_phone(strip):
     if result == 1:
         raise KeyError
     else:
-        return result
+        return f"Contact's phone is {result}"
 
 
 def input_error(func):
     def inner(*args, **kwargs):
-            try:
-                func(*args, **kwargs)
-            except KeyError:
-                print('No phone for this contact, try again')
-                inner(*args, **kwargs)
-            except ValueError:
-                print('Enter correct command')
-                inner(*args, **kwargs)
-            except IndexError:
-                print('Enter correct username or phone')
-                inner(*args, **kwargs)
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            inner(*args, **kwargs)
+            return 'No phone for this contact, try again'
+        except ValueError:
+            inner(*args, **kwargs)
+            return 'Enter correct command'
+        except IndexError:
+            inner(*args, **kwargs)
+            return 'Enter correct username or phone'
     return inner
+
+
+commands = {'hello': hello,
+            'add': add_or_change_phone,
+            'change': add_or_change_phone,
+            'phone': get_phone,
+            'show all': show_all,
+            'good bye': close,
+            'close': close,
+            'exit': close}
 
 
 @input_error
@@ -38,22 +64,15 @@ def main():
     while True:
         user_input = input('Enter your command: ')
         command = user_input.lower()
-        if command.startswith('hello'):
-            print('How can I help you?')
-        elif command.startswith('add') or command.startswith('change'):
-            add_or_change_phone(command)
-            print('Phone saved')
-        elif command.startswith('phone'):
-            get_phone_message = get_phone(command)
-            print(f"Contact's phone is {get_phone_message}")
-        elif command.startswith('show all'):
-            for key, value in phone_book.items():
-                print('|{:^15}|{:^15}|'.format(key, value))
-        elif command.startswith('good bye') or command.startswith('close') or command.startswith('exit'):
-            print("Good bye!")
-            break
+        commands_check = command.split(' ')
+        if commands_check[0] in commands:
+            result = commands[commands_check[0]]
+            print(result(command))
+            if command in ['good bye', 'close', 'exit']:
+                break
         else:
             raise ValueError
 
 
-main()
+output = main()
+print(output)
