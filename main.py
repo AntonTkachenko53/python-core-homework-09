0413a82021914cc217c9f054ddc58cfd23a09ff2
AntonTkachenko53 <gogochristofour@gmail.com>
2023-09-12 23:08:09 +0300

    fix 0.0.2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def hello(*args):
     return 'How can I help you?'
 
 
-def close(*args):
+def close():
     return "Good bye!"
 
 
@@ -13,7 +13,7 @@ def show_all(*args):
     result = ''
     for key, value in phone_book.items():
         result = result + '|{:^15}|{:^15}|\n'.format(key, value)
-    return result
+    return result.rstrip()
 
 
 def add_or_change_phone(strip):
@@ -29,6 +29,8 @@ def get_phone(strip):
     result = phone_book.get(words[1], 1)
     if result == 1:
         raise KeyError
+    elif len(words) != 2:
+        raise ValueError
     else:
         return f"Contact's phone is {result}"
 
@@ -38,14 +40,14 @@ def input_error(func):
         try:
             return func(*args, **kwargs)
         except KeyError:
+            print('No phone for this contact, try again')
             inner(*args, **kwargs)
-            return 'No phone for this contact, try again'
         except ValueError:
+            print('Enter correct command')
             inner(*args, **kwargs)
-            return 'Enter correct command'
         except IndexError:
+            print('Enter correct username or phone')
             inner(*args, **kwargs)
-            return 'Enter correct username or phone'
     return inner
 
 
@@ -54,9 +56,7 @@ commands = {'hello': hello,
             'change': add_or_change_phone,
             'phone': get_phone,
             'show all': show_all,
-            'good bye': close,
-            'close': close,
-            'exit': close}
+            }
 
 
 @input_error
@@ -64,15 +64,20 @@ def main():
     while True:
         user_input = input('Enter your command: ')
         command = user_input.lower()
-        commands_check = command.split(' ')
-        if commands_check[0] in commands:
-            result = commands[commands_check[0]]
-            print(result(command))
-            if command in ['good bye', 'close', 'exit']:
+        result = ''
+        output = None
+        if command in ['good bye', 'close', 'exit']:
+            print(close())
+            break
+        for char in command:
+            result += char
+            if result in commands:
+                output = commands[result](command)
                 break
-        else:
+        if output is None:
             raise ValueError
+        else:
+            print(output)
 
 
-output = main()
-print(output)
+main()
